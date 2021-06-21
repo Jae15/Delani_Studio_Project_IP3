9a51daf38ea3c7e78d00b90d2257df73f6f82b4c
Janet Mwangi <janomwangi@gmail.com>
2021-06-21 11:13:55 +0300

    Adjusted jquery code

--- script/script.js
+++ script/script.js
@@ -1,25 +1,44 @@
-$document.$(document).ready(function () {
-    $("div#design").hide();
-    $("div#development").hide();
-    $("div#product").hide();
+// $(document).ready(function () {
+//     $("div#design-content").hide();
+//     $("div#development").hide();
+//     $("div#product").hide();
 
-    $("imgdesign, div#design").click(function () { 
-        $("imgdesign, div#design").toggle();
+    // $("imgdesign, div#design").click(function () { 
+    //     $("imgdesign, div#design").toggle();
+
+    // });
+
+    // $("imgdevelopment, div#development").click(function () { 
+    //     $("imgdevelopment, div#development").toggle();
+
+    // });
+
+    // $("imgproduct, div#product").click(function () { 
+    //     $("imgproduct, div#product".toggle();
+
+    // });
+
+// });
+
+// document.getElementById("btn").addEventListener("click".function() {
+//     alert(Thank you for getting in touch with us!);
+
+// })\
+
+$(document).ready(function () {
+    $("div.design-content").hide();
+    
+       $("#imgdesign, div.design").click(function () { 
+        $("div.design, #imgdesign").toggle();
+
+
 
     });
 
-    $("imgdevelopment, div#development").click(function () { 
-        $("imgdevelopment, div#development").toggle();
-
+    
+    
+    
+    
+    
+    
     });
-
-    $("imgproduct, div#product").click(function () { 
-        $("imgproduct, div#product".toggle();
-
-    });
-
-});
-
-document.getElementById("btn").addEventListener("click".function() {
-    alert(Thank you for getting in touch with us!);
-})
